ebdf8683b2cd044d300805611792d83d0013458d
websites-webd <76954683+websites-webd@users.noreply.github.com>
2021-01-13 16:28:54 +0530

    Update index.js

--- project#3/index.js
+++ project#3/index.js
@@ -9,11 +9,6 @@ for(var i=0; i<document.querySelectorAll(".drum").length; i++ )
   });
 }
 
-// for key press on keyboard
-document.addEventListener("keypress",function(event){
-    makeSound(event.key);
-    btnAnimation(event.key);
-});
 
 function btnAnimation(key){
   document.querySelector("." + key).classList.add("pressed");
